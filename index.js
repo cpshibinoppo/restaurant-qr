df5e9bacfd4e9854d25617c2515ef8f573d790dc
cpshibinoppo <cpshibinoppo@gmail.com>
2022-07-30 12:37:59 +0530

    final output

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-import './news-article.js';
-import { topHeadlinesUrl } from './newsApi.js';
+import "./news-article.js";
+import { topHeadlinesUrl } from "./newsApi.js";
 
-window.addEventListener('load', () => {
+window.addEventListener("load", () => {
   getNews();
   registerSW();
 });
@@ -10,65 +10,65 @@ async function getNews() {
   const res = await fetch(topHeadlinesUrl);
   const json = await res.json();
 
-  const main = document.querySelector('main');
+  const main = document.querySelector("main");
 
-  json.articles.forEach(article => {
-    const el = document.createElement('news-article');
+  json.articles.forEach((article) => {
+    const el = document.createElement("news-article");
     el.article = article;
     main.appendChild(el);
   });
 }
 
 async function registerSW() {
-  if ('serviceWorker' in navigator) {
+  if ("serviceWorker" in navigator) {
     try {
-      await navigator.serviceWorker.register('./sw.js');
+      await navigator.serviceWorker.register("./sw.js");
     } catch (e) {
       console.log(`SW registration failed`);
     }
   }
 }
 
-const tabs = document.querySelectorAll('[data-tab-target]')
-const tabContents = document.querySelectorAll('[data-tab-content]')
+const tabs = document.querySelectorAll("[data-tab-target]");
+const tabContents = document.querySelectorAll("[data-tab-content]");
 
-tabs.forEach(tab => {
-  tab.addEventListener('click', () => {
-    const target = document.querySelector(tab.dataset.tabTarget)
-    tabContents.forEach(tabContent => {
-      tabContent.classList.remove('active')
-    })
-    tabs.forEach(tab => {
-      tab.classList.remove('active')
-    })
-    tab.classList.add('active')
-    target.classList.add('active')
-  })
-})
+tabs.forEach((tab) => {
+  tab.addEventListener("click", () => {
+    const target = document.querySelector(tab.dataset.tabTarget);
+    tabContents.forEach((tabContent) => {
+      tabContent.classList.remove("active");
+    });
+    tabs.forEach((tab) => {
+      tab.classList.remove("active");
+    });
+    tab.classList.add("active");
+    target.classList.add("active");
+  });
+});
 
 var swiper = new Swiper(".mySwiper", {
-    spaceBetween: 30,
-    centeredSlides: true,
-    autoplay: {
-      delay: 2500,
-      disableOnInteraction: false,
-    },
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-  });
-function call(){
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+function call() {
   // console.log("call");
   Swal.fire({
-    position: 'top-end',
-    icon: 'success',
-    title: 'Your work has been saved',
+    position: "top-end",
+    icon: "success",
+    title: "Your work has been saved",
     showConfirmButton: false,
-    timer: 1500
+    timer: 1500,
   });
 }
--- main.js
+++ main.js
@@ -1,19 +1,16 @@
 function on() {
-    document.getElementById("overlay").style.display = "block";
-  }
-  
-  function off() {
-    document.getElementById("overlay").style.display = "none";
-    console.log("off");
-  }
-function closeimg() { 
-  document.getElementById('myModal').style.display='none';
+  document.getElementById("overlay").style.display = "block";
+}
+function off() {
+  document.getElementById("overlay").style.display = "none";
+}
+function closeimg() {
+  document.getElementById("myModal").style.display = "none";
 }
 
 var modal = document.getElementById("myModal");
 var modalImg = document.getElementById("img01");
- function sent(src){
+function sent(src) {
   document.getElementById("myModal").style.display = "block";
   document.getElementById("img01").src = src;
-
 }

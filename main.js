18024ff1df14b7c58d17b3152a944ed42741776f
cpshibinoppo <cpshibinoppo@gmail.com>
2022-07-30 12:30:37 +0530

    fix bug of img pop

--- main.js
+++ main.js
@@ -6,29 +6,14 @@ function on() {
     document.getElementById("overlay").style.display = "none";
     console.log("off");
   }
-
-  // Get the modal
-var modal = document.getElementById("myModal");
-
-// Get the image and insert it inside the modal - use its "alt" text as a caption
-var img = document.getElementById("myImg");
-var modalImg = document.getElementById("img01");
-// img.onclick = function click(){
-//     console.log("imgon");
-//   modal.style.display = "block";
-//   modalImg.src = this.src;
-//   captionText.innerHTML = this.alt;
-// }
-function sent(){
-    console.log("addimg");
-    document.getElementById('myModal').style.display='block';
-    document.getElementById("img01").src = document.getElementById("myImg").src;
-    console.log(document.getElementById("myImg").src)
-}
-
-// Get the <span> element that closes the modal
-
-// When the user clicks on <span> (x), close the modal
 function closeimg() { 
   document.getElementById('myModal').style.display='none';
+}
+
+var modal = document.getElementById("myModal");
+var modalImg = document.getElementById("img01");
+ function sent(src){
+  document.getElementById("myModal").style.display = "block";
+  document.getElementById("img01").src = src;
+
 }

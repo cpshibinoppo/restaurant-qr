c7b29682e4f99455efab49eacc07319d2e7d2261
cpshibinoppo <cpshibinoppo@gmail.com>
2022-07-30 11:48:07 +0530

    add close

--- main.js
+++ main.js
@@ -30,6 +30,6 @@ function sent(){
 var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() { 
-  modal.style.display = "none";
+function closeimg() { 
+  document.getElementById('myModal').style.display='none';
 }
